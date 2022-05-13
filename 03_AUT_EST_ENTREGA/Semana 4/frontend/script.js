b707626eca26e22cc6afcb3db897a4c23fa7a466
gabreurt <gabriel.torres@sou.inteli.edu.br>
2022-05-13 20:21:49 -0300

    Adição de JavaScript no currículo!

---
+++ 03_AUT_EST_ENTREGA/Semana 4/frontend/script.js
@@ -0,0 +1,6 @@
+function Javascript() 
+{
+    window.location.href = "file:///C:/Users/bielt/OneDrive/%C3%81rea%20de%20Trabalho/Nova%20pasta/frontend/segundapag.html";
+}
+document.getElementById("botao").onclick = Javascript
+
---
+++ 03_AUT_EST_ENTREGA/Semana 4/frontend/script2.js
@@ -0,0 +1,6 @@
+document.getElementById("buttton").onclick = Alerta
+
+function Alerta() 
+{
+    alert("Infelizmente este menino já se encontra contratado, volte em novembro.")
+}
