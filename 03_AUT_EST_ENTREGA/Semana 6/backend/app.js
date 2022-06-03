a83b2b1daf41cff6e3816c39392293e82b1b37ce
gabreurt <gabriel.torres@sou.inteli.edu.br>
2022-06-03 20:23:33 -0300

    Adição do curriculo na semana 6

---
+++ 03_AUT_EST_ENTREGA/Semana 6/backend/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser"); // divide os dados (facilita para entender)
+const urlencondedParser = bodyParser.urlencoded({ extended: false });
+const sqlite3 = require("sqlite3").verbose();
+// comando específico do sqlite para aceitação das mensagens de uma forma facilitada
+const DBPATH = "lala.db";
+
+const hostname = "127.0.0.1";
+const port = 3071;
+
+app.get("/users", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Isso é importante para evitar o erro de CORS
+
+  var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = "SELECT * FROM curriculo ORDER BY id COLLATE NOCASE";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+  });
+  db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+  //app escuta a porta e o hostname (deixa o servidor rodando)
+  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
+
+app.use(express.static("../frontend/"));
