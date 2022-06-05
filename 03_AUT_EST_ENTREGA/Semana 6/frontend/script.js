ce321884fc78b175f974acf9f55307e9f448d945
gabreurt <gabriel.torres@sou.inteli.edu.br>
2022-06-04 21:30:15 -0300

    Correção da correção

--- 03_AUT_EST_ENTREGA/Semana 6/frontend/script.js
+++ 03_AUT_EST_ENTREGA/Semana 6/frontend/script.js
@@ -1,39 +1,35 @@
-const { append } = require("express/lib/response");
+function Javascript() 
+{
+    window.location.href = "\segundapag.html";
+};
 
-function Javascript() {
-  window.location.href = "segundapag.html";
-}
 
-function Alerta() {
-  alert(
-    "Infelizmente este menino já se encontra contratado, volte em novembro."
-  );
-}
+function Alerta() 
+{
+    alert("Infelizmente este menino já se encontra contratado, volte em novembro.")
+};
 
-let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = document.querySelector("#dark-mode-toggle");
-darkModeToggle.addEventListener("click", () => {
-  darkMode = localStorage.getItem("darkMode");
-  if (darkMode !== "enabled" || darkMode == null) {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled' || darkMode == null) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
 });
 
 const enableDarkMode = () => {
-  document.body.classList.add("darkmode");
-  localStorage.setItem("darkMode", "enabled");
-};
-
-const disableDarkMode = () => {
-  document.body.classList.remove("darkmode");
-  localStorage.setItem("darkMode", null);
-};
-
-if (darkMode === "enabled") {
-  enableDarkMode();
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
 }
 
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+}
 
-
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
